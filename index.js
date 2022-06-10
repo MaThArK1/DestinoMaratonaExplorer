56afb758bd5b4737f97c523e47aafc62a70a2530
matheus_barucho <matheus.barucho@tecnospeed.com.br>
2022-06-10 17:02:17 -0300

    Finalização da maratona

---
+++ index.js
@@ -0,0 +1,54 @@
+//Procura o elemento de id 'Resposta' no html
+const respostaElement = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+
+//clicar em fazer pergunta
+function fazerPergunta() {
+
+  if(inputPergunta.value == ""){
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+  //gerar numero aleatório
+  const totalRespostas = respostas.length
+  const i = Math.floor (Math.random()*totalRespostas)
+
+  respostaElement.innerHTML = pergunta + respostas[i]
+
+  respostaElement.style.opacity = 1;
+  
+  //sumir a resposta depois de 3 seg
+  setTimeout(function() {
+    respostaElement.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disabled");
+  }, 3000)
+}
